8e55737380386f323f2ed0f4bc9c0a82c6e889b0
albertzhao <szdxzhy@outlook.com>
2022-09-08 16:06:21 +0800

    3.1 ABPvNext框架介绍

---
+++ 3 ABPvNext/3.1 ABP.vNext框架介绍/AlbertPrintService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+using Volo.Abp.DependencyInjection;
+
+namespace _3._1_ABP.vNext框架介绍
+{
+    internal class AlbertPrintService:IScopedDependency
+    {
+        public string ReadFile()
+        {
+            return File.ReadAllText($"{AppDomain.CurrentDomain.BaseDirectory}3.1 ABP.vNext框架介绍.deps.json");
+        }
+    }
+}
---
+++ 3 ABPvNext/3.1 ABP.vNext框架介绍/Program.cs
@@ -0,0 +1,13 @@
+﻿using _3._1_ABP.vNext框架介绍;
+using Microsoft.Extensions.DependencyInjection;
+using Volo.Abp;
+
+using (var app=AbpApplicationFactory.Create<AppModule>(opt=>
+       {
+           opt.UseAutofac();
+       }))
+{
+    app.Initialize();
+
+    Console.WriteLine( app.Services.GetRequiredService<AlbertPrintService>().ReadFile());
+}

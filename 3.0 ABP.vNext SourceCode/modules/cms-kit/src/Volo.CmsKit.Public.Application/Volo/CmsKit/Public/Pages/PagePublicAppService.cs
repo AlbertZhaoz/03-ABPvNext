ef3fe175c6e5ade00d20aa21fb42368374983b05
albertzhaoz <szdxzhy@outlook.com>
2022-09-14 20:27:44 +0800

    增加 ABP.vNext 项目源码

---
+++ 3.0 ABP.vNext SourceCode/modules/cms-kit/src/Volo.CmsKit.Public.Application/Volo/CmsKit/Public/Pages/PagePublicAppService.cs
@@ -0,0 +1,33 @@
+﻿using System.Threading.Tasks;
+using Volo.Abp.Features;
+using Volo.Abp.GlobalFeatures;
+using Volo.CmsKit.Contents;
+using Volo.CmsKit.Features;
+using Volo.CmsKit.GlobalFeatures;
+using Volo.CmsKit.Pages;
+
+namespace Volo.CmsKit.Public.Pages;
+
+[RequiresFeature(CmsKitFeatures.PageEnable)]
+[RequiresGlobalFeature(typeof(PagesFeature))]
+public class PagePublicAppService : CmsKitPublicAppServiceBase, IPagePublicAppService
+{
+    protected IPageRepository PageRepository { get; }
+
+    public PagePublicAppService(IPageRepository pageRepository)
+    {
+        PageRepository = pageRepository;
+    }
+
+    public virtual async Task<PageDto> FindBySlugAsync(string slug)
+    {
+        var page = await PageRepository.FindBySlugAsync(slug);
+
+        if (page == null)
+        {
+            return null;
+        }
+
+        return ObjectMapper.Map<Page, PageDto>(page);
+    }
+}
---
+++ 3.0 ABP.vNext SourceCode/modules/feature-management/src/Volo.Abp.FeatureManagement.Domain.Shared/Volo/Abp/FeatureManagement/AbpFeatureManagementDomainSharedModule.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Extensions.DependencyInjection;
+using Volo.Abp.FeatureManagement.JsonConverters;
+using Volo.Abp.FeatureManagement.Localization;
+using Volo.Abp.Json;
+using Volo.Abp.Json.Newtonsoft;
+using Volo.Abp.Json.SystemTextJson;
+using Volo.Abp.Localization;
+using Volo.Abp.Localization.ExceptionHandling;
+using Volo.Abp.Modularity;
+using Volo.Abp.Validation;
+using Volo.Abp.Validation.Localization;
+using Volo.Abp.VirtualFileSystem;
+
+namespace Volo.Abp.FeatureManagement;
+
+[DependsOn(
+    typeof(AbpValidationModule),
+    typeof(AbpJsonModule)
+)]
+public class AbpFeatureManagementDomainSharedModule : AbpModule
+{
+    public override void ConfigureServices(ServiceConfigurationContext context)
+    {
+        Configure<AbpVirtualFileSystemOptions>(options =>
+        {
+            options.FileSets.AddEmbedded<AbpFeatureManagementDomainSharedModule>();
+        });
+
+        Configure<AbpLocalizationOptions>(options =>
+        {
+            options.Resources
+                .Add<AbpFeatureManagementResource>("en")
+                .AddBaseTypes(
+                    typeof(AbpValidationResource)
+                ).AddVirtualJson("Volo/Abp/FeatureManagement/Localization/Domain");
+        });
+
+        Configure<AbpExceptionLocalizationOptions>(options =>
+        {
+            options.MapCodeNamespace("Volo.Abp.FeatureManagement", typeof(AbpFeatureManagementResource));
+        });
+
+        Configure<AbpNewtonsoftJsonSerializerOptions>(options =>
+        {
+            options.Converters.Add<NewtonsoftStringValueTypeJsonConverter>();
+        });
+
+        var valueValidatorFactoryOptions = context.Services.GetPreConfigureActions<ValueValidatorFactoryOptions>();
+        Configure<ValueValidatorFactoryOptions>(options =>
+        {
+            valueValidatorFactoryOptions.Configure(options);
+        });
+
+        Configure<AbpSystemTextJsonSerializerOptions>(options =>
+        {
+            options.JsonSerializerOptions.Converters.Add(new StringValueTypeJsonConverter(valueValidatorFactoryOptions.Configure()));
+        });
+    }
+}

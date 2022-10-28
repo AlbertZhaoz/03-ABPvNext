966df2e9effceec53b3cabf3cef01eb46f91865a
albertzhao <szdxzhy@outlook.com>
2022-10-28 16:07:52 +0800

    update

--- 3.1 ABP.vNext框架介绍/src/Albert.Abp.WebApi/Program.cs
+++ 3.1 ABP.vNext框架介绍/src/Albert.Abp.WebApi/Program.cs
@@ -4,6 +4,9 @@ using Volo.Abp;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+// 模块类加载是在 AddApplication 中被加载的（源码阅读）
+// 模块类加载:https://github.com/AlbertZhaoz/03-ABPvNext/blob/master/3.0%20ABP.vNext%20SourceCode/framework/src/Volo.Abp.Core/Volo/Abp/Modularity/ModuleLoader.cs
+// AddApplication扩展方法:https://github.com/AlbertZhaoz/03-ABPvNext/blob/master/3.0%20ABP.vNext%20SourceCode/framework/src/Volo.Abp.Core/Microsoft/Extensions/DependencyInjection/ServiceCollectionApplicationExtensions.cs
 builder.Services.AddApplication<AppModule>(opt=>
 {
     // 替换默认配置
